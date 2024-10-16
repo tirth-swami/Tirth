792f6a524aa2e0ef2c87b4a8e3d1224b26f94f63
tirth-swami <tirthswami03@gmail.com>
2024-10-16 12:49:14 +0530

    Update Hero.jsx
    resume upload

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () =>
             JavaScript and basic React</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-                <div className="hero-resume">My Resume</div>
+                <div className="hero-resume"><a href="../tirth.pdf">My Resume</a></div>
             </div>
 
           </div>
